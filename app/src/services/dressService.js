cc4e4983cc75801205e5ef34f9bf6d414c2dbadc
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2025-03-04 18:28:12 +0200

    Update URL

--- app/src/services/dressService.js
+++ app/src/services/dressService.js
@@ -1,7 +1,7 @@
 // src/services/dressService.js
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000"; // כתובת השרת
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const fetchDresses = async () => {
     const response = await fetch(`${API_URL}/dresses`);
