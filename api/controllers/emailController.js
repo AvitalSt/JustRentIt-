0123040683b4f1a8981454fbe9f53ad68daba85b
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2025-09-28 14:22:20 +0300

    u p

--- api/controllers/emailController.js
+++ api/controllers/emailController.js
@@ -2,10 +2,12 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS
   },
 });
 
